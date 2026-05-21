b5f981e2a5169dd6e1e02e589ece665f1a2a689d
MhmdNat <mhmdnatour06gmail.com>
2026-05-21 16:41:55 +0300

    added file extension brute forcing functionality

--- directory_buster/directory_buster.py
+++ directory_buster/directory_buster.py
@@ -22,8 +22,6 @@ Muhammad Al Natour
 """
 
 import argparse
-from io import BytesIO
-from itertools import repeat
 import pycurl 
 from concurrent.futures import ThreadPoolExecutor
 from threading import Lock
@@ -44,22 +42,26 @@ class Config:
     filter_codes = []
     max_depth = 0
     threads = 0
+    extension = ['']
 
     @classmethod
     def initialize(cls, args, wordlist_data):
         cls.url = args.url
-        cls.url = cls.url + '/' if not cls.url.endswith('/') else cls.url
+        cls.url = cls.url + '/' if not cls.url.endswith('/') else cls.url # e.g. http://example.com -> http://example.com/
         cls.wordlist = wordlist_data
         cls.match_codes = args.match_code
         cls.filter_codes = args.filter_code
         cls.max_depth = args.max_depth
         cls.threads = args.threads
+        # args.extension is a flat list when passed as: -e .php .txt
+        cls.extension = args.extension 
 
 
 def get_status_code(path : str):
     '''Perform a curl request to the given path appended to base URL.'''
     c=pycurl.Curl()
-    c.setopt(c.URL, f'{Config.url}{path}/')
+    url = f'{Config.url}{path}'
+    c.setopt(c.URL, url)
     c.setopt(c.NOBODY, True)
     c.perform()
     status_code = c.getinfo(pycurl.RESPONSE_CODE)
@@ -67,19 +69,30 @@ def get_status_code(path : str):
     return status_code
 
 
-def check_path(new_path : str, depth: int):
+def check_path(new_path : str):
     '''Check a single path and return status code.'''
-    status_code = get_status_code(new_path)
+    results = ''
+    for ext in (Config.extension): # also checks as a directory
+        full_path = f'{new_path}{ext}/' if ext == '' else f'{new_path}{ext}'
 
-    if status_code in Config.match_codes:
-        print(f'{green}[+]{reset} {Config.url}{new_path} (Status Code: {green}{status_code}{reset})')
-        return status_code
-    elif status_code not in Config.filter_codes:
-        print(f'{red}[-]{reset} {Config.url}{new_path} (Status Code: {red}{status_code}{reset})')
-    return status_code    
+        status_code = get_status_code(full_path)
+
+        if status_code in Config.match_codes:
+            results += f'{green}[+]{reset} {Config.url}{full_path} (Status Code: {green}{status_code}{reset})\n'
+        elif status_code not in Config.filter_codes:
+            results += f'{red}[-]{reset} {Config.url}{full_path} (Status Code: {red}{status_code}{reset})\n'
+
+    if results:
+        print(results.strip())
+    return status_code # important in the case of directory brute forcing to know if we should explore child directories
 
 
-
+def push_child_directories(path : str, depth : int):
+    if depth < Config.max_depth:
+                with visited_lock:
+                    if path not in visited:
+                        visited.add(path)
+                        path_stack.put((path + '/', depth + 1, 0))
 
 
 def worker():
@@ -95,20 +108,16 @@ def worker():
         
         word = Config.wordlist[i]
         new_path = f'{path}{word}' if path else word
-        
+
         # Push continuation for next word in this directory
         if i + 1 < len(Config.wordlist):
             path_stack.put((path, depth, i + 1))
-        
         # Check this word
-        status_code = check_path(new_path, depth)
+        status_code = check_path(new_path)
         
-        # If found directory, push child directory scan
-        if status_code in Config.match_codes and depth < Config.max_depth:
-            with visited_lock:
-                if new_path not in visited:
-                    visited.add(new_path)
-                    path_stack.put((new_path + '/', depth + 1, 0))
+        # If found directory, and not file brute forcing, push child directory
+        if status_code in Config.match_codes and Config.extension==['']:
+            push_child_directories(new_path, depth)
 
 
 def main():
@@ -118,15 +127,16 @@ def main():
     parser.add_argument('-t', '--threads', help='Number of threads to use (default: 40)', type=int, default=40)
     parser.add_argument('-mc', '--match-code', help='HTTP status code to match (default: 200, 302)', nargs='+', type=int, default=[200,302])
     parser.add_argument('-fc', '--filter-code', help='HTTP status code to filter out (default: 404)', nargs='+', type=int, default=[404])
-    parser.add_argument('-max', '--max-depth', help='Maximum depth to explore (default: 5)', type=int, default=5)
+    parser.add_argument('--max-depth', help='Maximum depth to explore (/ is 0, default is 5)', type=int, default=5)
+    parser.add_argument('-e', '--extension', help='File extension to append to each word default checks directories (e.g. .php)', nargs='+', type=str, default=[''])
     args = parser.parse_args()
 
+    
     wordlist_path = args.wordlist
     with open(wordlist_path, 'r') as file:
         wordlist_data = [line.strip() for line in file.readlines() if line.strip()]
 
     Config.initialize(args, wordlist_data)
-
     # Reset stack and visited for fresh scan
     while not path_stack.empty():
         try:
